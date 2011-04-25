27291627d2fa4b34dfa683e2c051098b90b27463
Jobin <johnjobin@gmail.com>
2011-04-25 22:00:50 +0530

    ~Adjust.~

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,7 +8,8 @@ class WelcomeController < ApplicationController
 #	@d="jobin1"
 # @categories[][]
 
-#puts "Controller"
+puts "Controller"
+
 
 #svc = OData::Service.new "http://odata.netflix.com/Catalog/"
 #svc.Titles#.filter("Name eq 'Office Space'")
@@ -18,11 +19,10 @@ class WelcomeController < ApplicationController
 
 #  carriercollection = []
 #categories = OData::QueryBuilder.new
-  svc = OData::Service.new "http://vmw3815.wdf.sap.corp:50009/sap/opu/sdata/iwfnd/RMTSAMPLEFLIGHT", { :username => "naidupu", :password=> "punaidu" }
-#  svc = OData::Service.new "http://localhost:8080/sap/opu/sdata/iwfnd/RMTSAMPLEFLIGHT", { :username => "naidupu", :password=> "punaidu" }
-  puts "Responds to FlightCollection? #{svc.respond_to? :CarrierCollection}" # => Responds to FlightCollection? true
-  svc.CarrierCollection
- @flightcollection = svc.execute
+
+#  puts "Responds to FlightCollection? #{svc.respond_to? :CarrierCollection}" # => Responds to FlightCollection? true
+#  svc.CarrierCollection
+# @flightcollection = svc.execute
  # puts categories.to_json
   end
 
